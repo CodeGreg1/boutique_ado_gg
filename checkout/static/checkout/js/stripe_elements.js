7e5e688d6c30c069826d0d4771043758764e7631
GregWebDev <code@greggoodrem.co.uk>
2023-01-07 22:54:43 +0000

    added stripe elements2

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -3,8 +3,6 @@ var stripe_public_key = $('id_stripe_public_key').text().slice(1, -1)
 var client_secret = $('id_client_secret').text().slice(1, -1)
 var stripe = Stripe(stripe_public_key)
 var elements = stripe.elements()
-var card = elements.create('card', {style: style})
-
 var style = {
     base: {
         color: '#000',
@@ -20,5 +18,6 @@ var style = {
         iconColor: '#dc3545'
     }
 }
+var card = elements.create('card', {style: style})
 
 card.mount('#card-element')
